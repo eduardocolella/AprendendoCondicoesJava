00f25468b52072d43b00bb6d819d3445c4b12c32
Eduardo <eduardo310301@edu.unipar.br>
2024-08-21 20:49:41 -0300

    Criação do projeto java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer idadePessoa = 23;
+        String nomePessoa = "LittleRonald";
+
+        /*
+        * > Maior
+        * >= maior igual
+        * < Menor
+        * <= Menor igual
+        * == é igual
+        * != é diferente
+        * equal compara TExtos
+         */
+        if (idadePessoa > 21) {
+            System.out.println("é maior que 21");
+        }else{
+            System.out.println("Não é maior que 21");
+        }
     }
 }
